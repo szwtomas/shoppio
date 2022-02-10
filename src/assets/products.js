ef6552a26c6a9e225cc26b337791b4adb640c023
Tomas Szwarcberg <tszwarcberg@fi.uba.ar>
2022-02-10 16:54:33 -0300

    Simulates product fetch with timeout. Items are still not responsive

--- src/assets/products.js
+++ src/assets/products.js
@@ -27,4 +27,12 @@ const products = [
 	},
 ];
 
-export { products };
+const getProducts = () => {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve(products);
+		}, 2000);
+	});
+};
+
+export { getProducts };
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./itemList.css";
 import Item from "../Item";
-import { products } from "../../assets/products";
+import { getProducts } from "../../assets/products";
 
 const ItemList = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		getProducts()
+			.then(products => setProducts(products))
+			.catch(() => console.log("Error fetching products"));
+	}, []);
+
 	const getItemElements = () => {
 		return products.map(p => (
-			<Item title={p.title} price={p.price} imgUrl={p.imgUrl} />
+			<Item
+				title={p.title}
+				price={p.price}
+				imgUrl={p.imgUrl}
+				key={p.id}
+			/>
 		));
 	};
 
