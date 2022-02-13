b8aa19703cd00ac2a335b3559c49a05c17b0e48d
Tomas Szwarcberg <tszwarcberg@fi.uba.ar>
2022-02-13 17:20:22 -0300

    Adds responsive hamburger menu in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CartWidget from "../CartWidget";
 import "./navbar.css";
 
 const Navbar = () => {
+	const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+	const [showBarsNavbar, setShowBarsNavbar] = useState(false);
+
+	useEffect(() => {
+		const changeWidth = () => {
+			setScreenWidth(window.innerWidth);
+		};
+
+		window.addEventListener("resize", changeWidth);
+
+		return () => window.removeEventListener("resize", changeWidth);
+	}, []);
+
+	const toggleNavbar = () => {
+		setShowBarsNavbar(!showBarsNavbar);
+	};
+
+	useEffect(() => {
+		if (screenWidth >= 690) {
+			setShowBarsNavbar(false);
+		}
+	}, [screenWidth, setScreenWidth]);
+
 	return (
 		<div className="navbar-container">
-			<nav className="navbar">
-				<div className="navbar--brand">
+			<nav className={!showBarsNavbar ? "navbar" : "navbar navbar-bars"}>
+				<div
+					className={
+						!showBarsNavbar
+							? "navbar--brand"
+							: "navbar--brand brand-open"
+					}
+				>
 					<i className="fas fa-shopping-bag navbar--icon"></i>
 					<h2 className="navbar--title">Shoppio</h2>
 				</div>
@@ -20,9 +49,29 @@ const Navbar = () => {
 
 				<CartWidget className="navbar--cart" itemCount={0} />
 
-				<button className="navbar--bars">
-					<i className="fas fa-bars"></i>
-				</button>
+				{!showBarsNavbar && (
+					<button className="navbar--bars" onClick={toggleNavbar}>
+						<i className="fas fa-bars"></i>
+					</button>
+				)}
+				{showBarsNavbar && (
+					<button
+						className="navbar--close-btn"
+						onClick={toggleNavbar}
+					>
+						<i className="far fa-times"></i>
+					</button>
+				)}
+				{showBarsNavbar && (
+					<div className="navbar--menu-container">
+						<ul className="navbar--menu">
+							<li>Featured</li>
+							<li>Notebooks</li>
+							<li>Tablets</li>
+							<li>Cell Phones</li>
+						</ul>
+					</div>
+				)}
 			</nav>
 		</div>
 	);
